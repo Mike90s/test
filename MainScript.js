faf87d550f23c36d5e2757d1ce836ee9edf8a2c5
Mike <mihai.bercuci@yahoo.com>
2016-06-12 17:45:49 +0100

    Add files via upload

---
+++ MainScript.js
@@ -0,0 +1,23 @@
+﻿
+
+
+
+
+$(document).ready(function () {
+    
+  
+    $(".mobile-button").click(function () {
+        if ($("#MainMenu ul").hasClass("mobile-nav") != true) {
+            $("#MainMenu ul").addClass("mobile-nav");
+            $("#MainMenu ul").removeClass("main-nav");
+            $("#MainMenu ul").removeClass("menu");
+            $("#MainMenu ul").show();
+        }
+        else {
+            $("#MainMenu ul").removeClass("mobile-nav");
+            $("#MainMenu ul").hide();
+        }
+          
+        }
+   
+)});
